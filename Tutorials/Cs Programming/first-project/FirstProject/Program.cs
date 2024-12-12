25f2a94da8be57da87b3a5fa42c38ab9a7ed4c08
Sami Munir <sami.munir2001@gmail.com>
2024-12-12 07:53:55 -0500

    creating first C# project

---
+++ Tutorials/Cs Programming/first-project/FirstProject/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstProject {
+    internal class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("\nC# First Project\n");
+            Console.ReadLine();
+        }
+    }
+}
